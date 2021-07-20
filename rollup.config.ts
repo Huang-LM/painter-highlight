a83acf04fc071914d3a5b54eefee2b0f2e6b28e1
Huang-LM <57673015+Huang-LM@users.noreply.github.com>
2021-07-20 12:15:16 +0800

    test

---
+++ rollup.config.ts
@@ -0,0 +1,39 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import typescript from "rollup-plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+import esbuild from "rollup-plugin-esbuild";
+import babel from "rollup-plugin-babel";
+import { eslint } from "rollup-plugin-eslint";
+// import pkg from './package.json';
+
+const devPulugins = [
+  resolve(),
+  commonjs(),
+  typescript(),
+  eslint(),
+  // terser({ compress: { drop_console: true } }),
+  babel({ exclude: "node_modules/**" }),
+  esbuild({
+    include: /\.ts?$/,
+    exclude: /node_modules/,
+    sourceMap: false,
+    minify: false,
+  }),
+];
+
+export default [
+  {
+    input: "src/index.ts",
+
+    external: ["ms"],
+    output: [
+      {
+        file: "dist/index-es.js", // es6模块
+        name: "phl-es",
+        format: "es",
+      },
+    ],
+    plugins: devPulugins,
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-console.log("hello world!");
+import phl from "./main";
+
+export { phl };
