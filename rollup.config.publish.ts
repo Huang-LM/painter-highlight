9e8057936f0e6e31036b7bbf1658a7c2b3f223c4
Huang-LM <Huang-LM@users.noreply.github.com>
2021-07-21 11:10:49 +0800

    0.0.9 fix bug

--- rollup.config.publish.ts
+++ rollup.config.publish.ts
@@ -1,19 +1,21 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import typescript from "rollup-plugin-typescript";
-import { terser } from "rollup-plugin-terser";
-import esbuild from "rollup-plugin-esbuild";
-import babel from "rollup-plugin-babel";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import esbuild from 'rollup-plugin-esbuild';
+import babel from 'rollup-plugin-babel';
+import peerDependencies from 'rollup-plugin-peer-deps-external';
 // import { eslint } from "rollup-plugin-eslint";
 // import pkg from './package.json';
 
 const prodPulugins = [
+  peerDependencies(),
   resolve(), // 查找和打包node_modules中的第三方模块(因为rollup无法识别node_modules中的包)
   commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理（node_modules中的包大部分都是commonjs格式的，要在rollup中使用必须先转为ES6语法）
   typescript(), // 解析TypeScript
   terser({ compress: { drop_console: true } }),
   babel({
-    exclude: "node_modules/**", // 只编译我们的源代码
+    exclude: 'node_modules/**', // 只编译我们的源代码
   }),
   esbuild({
     include: /\.ts?$/,
@@ -25,15 +27,19 @@ const prodPulugins = [
 
 export default [
   {
-    input: "src/index.ts", // 打包入口
+    input: 'src/index.ts', // 打包入口
 
-    external: ["ms"],
+    external: ['ms', 'highlight.js', 'painter-kernel'],
     output: [
       // 打包出口
       {
-        file: "dist/index.js", // 通用模块
-        name: "phl",
-        format: "umd",
+        file: 'dist/index.js', // 通用模块
+        name: 'phl',
+        format: 'umd',
+        globals: {
+          'highlight.js': 'hljs',
+          'painter-kernel': 'painter',
+        },
       },
     ],
     plugins: prodPulugins,
--- rollup.config.ts
+++ rollup.config.ts
@@ -1,10 +1,11 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import typescript from "rollup-plugin-typescript";
-import { terser } from "rollup-plugin-terser";
-import esbuild from "rollup-plugin-esbuild";
-import babel from "rollup-plugin-babel";
-import { eslint } from "rollup-plugin-eslint";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript';
+// import { terser } from 'rollup-plugin-terser';
+import esbuild from 'rollup-plugin-esbuild';
+import babel from 'rollup-plugin-babel';
+import { eslint } from 'rollup-plugin-eslint';
+import peerDependencies from 'rollup-plugin-peer-deps-external';
 // import pkg from './package.json';
 
 const devPulugins = [
@@ -12,8 +13,9 @@ const devPulugins = [
   commonjs(),
   typescript(),
   eslint(),
+  peerDependencies(),
   // terser({ compress: { drop_console: true } }),
-  babel({ exclude: "node_modules/**" }),
+  babel({ exclude: '**/node_modules/**' }),
   esbuild({
     include: /\.ts?$/,
     exclude: /node_modules/,
@@ -24,14 +26,18 @@ const devPulugins = [
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
 
-    external: ["ms"],
+    external: ['ms', 'highlight.js', 'painter-kernel'],
     output: [
       {
-        file: "dist/index-es.js", // es6模块
-        name: "phl-es",
-        format: "es",
+        file: 'dist/index-es.js', // es6模块
+        name: 'phl-es',
+        format: 'es',
+        globals: {
+          'highlight.js': 'hljs',
+          'painter-kernel': 'painter',
+        },
       },
     ],
     plugins: devPulugins,
