56eca3394d0612187cd62a2cdd427e012e71d628
Huang-LM <Huang-LM@users.noreply.github.com>
2021-07-25 18:12:21 +0800

    Delete lib directory

--- lib/types/main.d.ts
+++
@@ -1,9 +0,0 @@
-declare type template = {
-    background: string;
-    width: string;
-    height: string;
-    borderRadius: string;
-    views: any;
-};
-declare const phl: (CanvasNode: object, canvas: CanvasRenderingContext2D, template: template, code: string, language: string) => void;
-export default phl;
